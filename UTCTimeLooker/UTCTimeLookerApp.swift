e127d32dcf92bf6f3428844efe4eba7e31079ed9
Levi Lei <levilei@esdlife.com>
2025-07-28 18:22:58 +0800

    Initial Commit

---
+++ UTCTimeLooker/UTCTimeLookerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UTCTimeLookerApp.swift
+//  UTCTimeLooker
+//
+//  Created by Lei Levi on 28/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct UTCTimeLookerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

ad212cbbadcceaab3b15f54c94466f7ab8627a82
Levi <lzkhaoshua@gmail.com>
2025-12-03 17:52:10 +0800

    feat: add Target for iPadOS

---
+++ UTCTimeLookeriPadOS/UTCTimeLookeriPadOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  UTCTimeLookeriPadOSApp.swift
+//  UTCTimeLookeriPadOS
+//
+//  Created by Lei Levi on 3/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct UTCTimeLookeriPadOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

c1f29f48f2691c7174777361f8eca50d0f7f22a7
Levi <lzkhaoshua@gmail.com>
2025-12-03 17:39:32 +0800

    feat: add iOS Target

---
+++ UTCTimeLookeriOS/UTCTimeLookeriOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  UTCTimeLookeriOSApp.swift
+//  UTCTimeLookeriOS
+//
+//  Created by Lei Levi on 3/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct UTCTimeLookeriOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

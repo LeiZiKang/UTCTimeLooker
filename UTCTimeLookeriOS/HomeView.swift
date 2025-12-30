fd415cc9eda2fc2bf1ccd796566534b68d1bb640
Levi <lzkhaoshua@gmail.com>
2025-12-30 10:43:01 +0800

    feat: add HomeView for iOS Target

---
+++ UTCTimeLookeriOS/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  UTCTimeLookeriOS
+//
+//  Created by Lei Levi on 4/12/2025.
+//
+
+import SwiftUI
+import UTCTimeCore
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
